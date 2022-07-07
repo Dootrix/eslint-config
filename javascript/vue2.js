97439b7fea13de636a61466e2c4a633a674233b0
Neil Sweeney <neil@enhko.com>
2022-07-07 11:25:05 +0100

    Update vue2 rules

--- javascript/vue2.js
+++ javascript/vue2.js
@@ -4,12 +4,5 @@ module.exports = {
   extends: ["plugin:vue/recommended"],
   rules: {
     ...rules,
-    "vue/html-closing-bracket-newline": [
-      "error",
-      {
-        singleline: "never",
-        multiline: "never",
-      },
-    ],
   },
 };
