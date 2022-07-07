cb851e8f1b3fcd4846a0b7fdfb6dc7fabfc73ce0
Neil Sweeney <neil@enhko.com>
2022-07-07 11:25:34 +0100

    Update project only eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,8 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['./javascript', './javascript/prettier'],
+  extends: ["./javascript", "./javascript/prettier"],
+  rules: {
+    "unicorn/prefer-module": "off",
+  },
 };
